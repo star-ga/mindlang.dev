6ba22c0757e30a526901f0a56cb3175cf8f4c001
Nikolai Nedovodin <info@cputer.com>
2025-11-21 03:32:24 +0000

    fix: restore missing index.njk and layout files

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "node_modules/sprucecss/dist/spruce.css": "css/spruce.css" });
-  eleventyConfig.addPassthroughCopy("src/css");
   return {
     dir: { input: "src", output: "dist", includes: "_includes" },
     templateFormats: ["njk", "md", "html"]
