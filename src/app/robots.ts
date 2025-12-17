77a6da5a2e3b26da525658842b2f0d8f82ddbd05
Nikolai Nedovodin <info@cputer.com>
2025-12-17 04:42:46 -0800

    Fix static export: Add force-static to sitemap.ts and robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,7 @@
 import { MetadataRoute } from 'next';
 import { siteConfig } from '@/data/site';
 
+export const dynamic = 'force-static';
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
