b959c6e0afc2fb6ab07ced14f72e559ef019995c
STARGA Inc. <noreply@star.ga>
2026-01-19 05:41:05 -0800

    chore: change security email from security@star.ga to info@star.ga

--- src/data/site.ts
+++ src/data/site.ts
@@ -11,7 +11,7 @@ export const siteConfig = {
   x: "https://x.com/mindlang",
   company: "STARGA, Inc.",
   contactEmail: "info@star.ga",
-  securityEmail: "security@star.ga",
+  securityEmail: "info@star.ga",
 };
 
 export type SiteConfig = typeof siteConfig;
